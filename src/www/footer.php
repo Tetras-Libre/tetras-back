2613a45db90de3f0447c72fbb11b197c369b7d4f
David Beniamine <david.beniamine@imag.fr>
2017-04-11 11:39:01 +0200

    fix broken paragraph

--- src/www/footer.php
+++ src/www/footer.php
@@ -2,6 +2,7 @@
 <p>
 <a href="https://gitlab.tetras-libre.fr/tetras-libre/Tetras-back">Tetras-back</a> est un logiciel modulaire et configurable de sauvegarde sur disque dur externe conçus pour les solutions NoCloud de <a href="https://tetras-libre.fr">Tetras Libre</a> et <a href="https://demo-tic.org">Demo-TIC</a>.
 </p>
+<p>
 <a href="https://gitlab.tetras-libre.fr/tetras-libre/Tetras-back">Tetras-back</a> a été développé par David Beniamine avec l'aide de Felix Coudurier et Sebastien Curt pour Tetras Libre et est distribué sous la licence libre <a href="https://www.gnu.org/licenses/gpl-3.0.txt">GPL V3.0</a>.
 </p>
 <p>
